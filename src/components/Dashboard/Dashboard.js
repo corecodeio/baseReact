9993025229a915d61088501498adfe5d711940f3
Yosef Maldonado Arriaga <yosef.maldonado@pccenter.com.gt>
2021-10-16 11:29:20 -0600

    last changes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -12,7 +12,31 @@ const Dashboard = () => {
   const tipoDeCuentaRef = useRef();
   const monedaRef = useRef();
 
-  console.log(authCtx.currentUser);
+  const listUserAccounts = async () => {
+    const idToken = await authCtx.currentUser.getIdToken();
+    let response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/account`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${idToken}`
+      }
+    });
+    let accounts = await response.json();
+    return accounts;
+  }
+
+  const createAccount = async (body) => {
+    const idToken = await authCtx.currentUser.getIdToken();
+    let response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/account`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${idToken}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+    let accountCreated = await response.json();
+    console.log(accountCreated);
+  }
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
@@ -20,17 +44,63 @@ const Dashboard = () => {
     if(tipoDeCuentaRef.current.value === '') return;
     if(monedaRef.current.value === '') return;
     setDisableSubmit(true)
-    // ...
-    //const idToken = await authCtx.currentUser.getIdToken();
+    const description = nombreCuentaRef.current.value;
+    const account_type = Number(tipoDeCuentaRef.current.value);
+    const currency = Number(monedaRef.current.value);
+    console.log(`Description: ${description}`);
+    console.log(`Account Type: ${account_type}`);
+    console.log(`Currency: ${currency}`);
+    try {
+      await createAccount({ description, account_type, currency });
+      const accounts = await listUserAccounts();
+      setUserAccounts(accounts.data);
+    } catch(e) {
+      console.log(e);
+    } 
     setDisableSubmit(false)
     nombreCuentaRef.current.value = '';
     tipoDeCuentaRef.current.value = '';
     monedaRef.current.value = '';
   }
 
-  const accountTypesOptions = null;
-  const currenciesOptions = null;
-  const userAccountsList = null;
+  useEffect(() => {
+    const getCurrenciesAndAccounTypes = async () => {
+      const idToken = await authCtx.currentUser.getIdToken();
+      let response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/currency`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${idToken}`
+        }
+      });
+      let currencies = await response.json();
+      response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/account-type`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${idToken}`
+        }
+      });
+      let accountTypes = await response.json();
+      setCurrencies(currencies.data);
+      setAccountTypes(accountTypes.data);
+    }
+    getCurrenciesAndAccounTypes();
+
+    const getAllUserAccounts = async () => {
+      const accounts = await listUserAccounts();
+      setUserAccounts(accounts.data);
+    }
+    getAllUserAccounts();
+
+  }, []);
+
+  const accountTypesOptions = accountTypes.map((option) => <option key={option.account_type} value={option.account_type}>{option.description}</option>);
+  const currenciesOptions = currencies.map((option) => <option key={option.currency} value={option.currency}>{option.description}</option>);
+  const userAccountsList = userAccounts.map((account) => {
+      return <ListGroup.Item key={account.account}>
+        <h3>{account.description}</h3>
+        <p><b>Currency:</b>{account.currency_description}, <b>Account Type:</b> {account.account_type_description}</p>
+      </ListGroup.Item>;
+  });
 
   return (
     <Container fluid className="mt-5">
